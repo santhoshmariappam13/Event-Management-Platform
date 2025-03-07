1e0bdb03843b99f4c2ebdc49d6eb8fab3ac64a2a
santhosh <santhoshmariappan13@gmail.com>
2025-03-07 21:29:19 +0530

    server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-require('dotenv').config();  // To load environment variables
+require('dotenv').config();  
 
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');  // Correctly import the connectDB function
+const connectDB = require('./config/db');  
 
 const userRoutes = require('./routes/userRoutes');
 const eventRoutes = require('./routes/eventRoutes');
@@ -13,14 +13,11 @@ const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Connect to MongoDB
-connectDB();  // Call the connectDB function here
 
-// Routes
+connectDB();  
 app.use('/api/users', userRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/payments', paymentRoutes);
@@ -28,6 +25,5 @@ app.use('/api/registrations', registrationRoutes);
 app.use('/api/tickets', ticketRoutes);
 app.use('/api/admin', adminRoutes);
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
